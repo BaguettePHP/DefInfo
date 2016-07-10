ceb03b86c987070348320e5b6ebe272a3e5e32e7
USAMI Kenta <tadsan@zonu.me>
2016-07-10 19:28:12 +0900

    Update

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 ```
 % phpdefinfo class DateTime
 % phpdefinfo class --format=json DateTime
-% phpdefinfo class --include=./vendor/autoload.php DateTime
+% phpdefinfo class --include=~/vendor/autoload.php DateTime
 % phpdefinfo class --format=json_pretty DateTime
 ```
--- load.php
+++ load.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * PhpDefInfo - loader
+ *
+ * @author    USAMI Kenta <tadsan@zonu.me>
+ * @copyright 2016 USAMI Kenta
+ * @license   MPL-2.0 https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 if (!function_exists('PhpDefInfo\getClass')) {
     require_once __DIR__ . '/src/definfo.php';
 }
